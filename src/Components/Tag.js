a823270b0e4fb55468242a136d6720d33eadcb91
JerryPC <jeremaios@gmail.com>
2020-12-14 15:07:53 -0300

    formatting tag.js

--- src/Components/Tag.js
+++ src/Components/Tag.js
@@ -1,36 +1,48 @@
- import React from 'react';
- import { useEffect } from 'react';
- import { useRef } from 'react';
+import React from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+/**
+ *
+ * @param {string} text - The name provided from input
+ * @param {number} x - x
+ * @param {number} y - y
+ * @param {number} radius - radius
+ * @param {number} startRotation - startRotations
+ */
+CanvasRenderingContext2D.prototype.fillTextCircle = function (
+  text,
+  x,
+  y,
+  radius,
+  startRotation
+) {
+  var numDegreesPerLetter = (2 * Math.PI) / text.length;
+  this.save();
+  this.translate(x, y);
+  this.rotate(startRotation);
 
+  for (var i = 0; i < text.length; i++) {
+    this.save();
+    this.translate(radius, 0);
+    //      if (i == 0) {
+    //          this.fillStyle = 'red';
+    this.translate(10, -10);
+    //          this.fillRect(0,0,4,4);
+    this.rotate(1.4);
+    this.translate(-10, 10);
+    //          this.fillStyle = 'black';
+    //      }
 
-CanvasRenderingContext2D.prototype.fillTextCircle = function(text,x,y,radius,startRotation){
-   var numDegreesPerLetter = 2*Math.PI / text.length;
-   this.save();
-   this.translate(x,y);
-   this.rotate(startRotation);
+    //      this.fillRect(0,0,4,4);
+    this.fillText(text[i], 0, 0);
+    this.restore();
+    this.rotate(numDegreesPerLetter);
+  }
+  this.restore();
+};
 
-   for(var i=0;i<text.length;i++){
-      this.save();
-      this.translate(radius, 0);
-//      if (i == 0) {
-//          this.fillStyle = 'red';
-          this.translate(10, -10);
-//          this.fillRect(0,0,4,4);
-          this.rotate(1.4)
-          this.translate(-10, 10);          
-//          this.fillStyle = 'black';
-//      }
-
-//      this.fillRect(0,0,4,4);
-      this.fillText(text[i],0,0);
-      this.restore();
-      this.rotate(numDegreesPerLetter);
-   }
-   this.restore();
-}
- 
-const getPixelRatio = context => {
-    var backingStore =
+const getPixelRatio = (context) => {
+  var backingStore =
     context.backingStorePixelRatio ||
     context.webkitBackingStorePixelRatio ||
     context.mozBackingStorePixelRatio ||
@@ -38,40 +50,31 @@ const getPixelRatio = context => {
     context.oBackingStorePixelRatio ||
     context.backingStorePixelRatio ||
     1;
-    
-    return (window.devicePixelRatio || 1) / backingStore;
+  return (window.devicePixelRatio || 1) / backingStore;
 };
- 
- const Tag = (props) => {
-     let ref = useRef();
-     
-     useEffect(() => {
-         let canvas = ref.current;
-         let ctx = canvas.getContext('2d');
-         
-        let ratio = getPixelRatio(ctx);
-        let width = getComputedStyle(canvas)
-            .getPropertyValue('width')
-            .slice(0, -2);
-        let height = getComputedStyle(canvas)
-            .getPropertyValue('height')
-            .slice(0, -2);
-         
-        canvas.width = width * ratio;
-        canvas.height = height * ratio;
-        canvas.style.width = `${width}px`;
-        canvas.style.height = `${height}px`;
-         
-        ctx.font = "bold 30px Serif";
-        ctx.fillTextCircle(props.name, 50,50,25,Math.PI / 2);
-     });
-     
-     return (
-         <canvas
-             ref={ref} 
-             style={{ width: '100px', height: '100px' }}
-         />
-     );
- };
- 
- export default Tag;
+
+const Tag = (props) => {
+  let ref = useRef();
+
+  useEffect(() => {
+    let canvas = ref.current;
+    let ctx = canvas.getContext('2d');
+
+    let ratio = getPixelRatio(ctx);
+    let width = getComputedStyle(canvas).getPropertyValue('width').slice(0, -2);
+    let height = getComputedStyle(canvas)
+      .getPropertyValue('height')
+      .slice(0, -2);
+
+    canvas.width = width * ratio;
+    canvas.height = height * ratio;
+    canvas.style.width = `${width}px`;
+    canvas.style.height = `${height}px`;
+
+    ctx.font = 'bold 30px Serif';
+    ctx.fillTextCircle(props.name, 50, 50, 25, Math.PI / 2);
+  });
+  return <canvas ref={ref} style={{ width: '160px', height: '160px' }} />;
+};
+
+export default Tag;
