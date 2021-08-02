9e9309b948171ba5c10f58439e1649f6a7c13c15
JerryLion-PC <jeremaios@gmail.com>
2021-08-02 10:14:56 -0300

    update register page

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -9,16 +9,18 @@ import { useHistory, useLocation } from 'react-router-dom';
 import Button from '../components/styleComponents/Button';
 import Footer from '../components/styleComponents/Footer';
 import RegisterForm from '../components/RegisterForm';
-// import SettingsButton from '../components/styleComponents/SettingsButton';
-// import LoadingComponent from '../components/styleComponents/LoadingComponent';
+import SettingsButton from '../components/styleComponents/SettingsButton';
+import LoadingComponent from '../components/styleComponents/LoadingComponent';
 
 // Styles
 import styles from '../styles/styles';
 
-export default function RegisterPage() {
+export default function RegisterPage({ showMessage }) {
   const auth = useAuth();
   const history = useHistory();
   const { email: receivedEmail } = useLocation().state || { email: '' };
+
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const handleChange = (update) => {
     const { input, value } = update;
@@ -40,9 +42,10 @@ export default function RegisterPage() {
   }, []);
 
   const handleRegister = () => {
+    setIsLoading(true);
     const { name, password, repeatedPassword, email, address } = userInput;
     if (password !== repeatedPassword) {
-      handleShowMessage({
+      showMessage({
         code: 'input/password-different',
         message: 'The two fields with password must be equal.',
       });
@@ -51,54 +54,40 @@ export default function RegisterPage() {
     auth
       .signUp(email, password, name, address)
       .then((user) => {
+        setIsLoading(false);
         history.push('/');
       })
       .catch((err) => {
-        handleShowMessage(err);
+        showMessage(err);
       });
   };
 
-  const modalReducer = (state, action) => {
-    switch (action.type) {
-      case 'trigger':
-        const { method, message, value, callback } = action;
-        return { showModal: true, method, message, value, callback };
-      case 'response':
-        if (action.callback) {
-          action.callback(action.value);
-        }
-        return { showModal: false };
-      default:
-        throw new Error();
-    }
-  };
-  const [modalState, modalDispatch] = React.useReducer(modalReducer, {
-    showModal: false,
-    method: null,
-    message: null,
-    value: null,
-    callback: null,
-  });
-
-  const handleShowMessage = (props) => {
-    const { code, message } = props;
-    switch (code) {
-      case 'input/password-different':
-      case 'auth/weak-password':
-      case 'auth/email-already-in-use':
-      case 'auth/invalid-email':
-        modalDispatch({ type: 'trigger', method: 'alert', message });
-        break;
-      default:
-        console.log(props);
-        break;
-    }
+  const signInWithGoogle = () => {
+    setIsLoading(true);
+    auth.signInWithGooglePopup(() => {
+      history.push('/');
+      setIsLoading(false);
+    });
   };
 
   return (
     <>
-      {/* <MessageModal state={modalState} dispatch={modalDispatch} /> */}
       <header style={styles.loginHeader}>
+        <div
+          style={{
+            minWidth: '270px',
+            maxWidth: '450px',
+            width: '80%',
+            height: '2vh',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'stretch',
+            flexDirection: 'row-reverse',
+          }}
+        >
+          <SettingsButton />
+          <Button onClick={() => history.push('/')} icon={'navigate_before'} />
+        </div>
         <img
           src={'../logoLogin.svg'}
           alt={''}
@@ -111,14 +100,44 @@ export default function RegisterPage() {
       <div style={styles.divFlexColumn}>
         <span style={styles.loginHeading1}>Register</span>
         <span style={styles.loginHeading2}>Hello! Please to meet you.</span>
-        <RegisterForm state={userInput} onChange={handleChange} />
-        <Button
-          style={styles.btnFilledPurple}
-          onClick={handleRegister}
-          icon={''}
-        >
-          Register
-        </Button>
+        {isLoading ? (
+          <LoadingComponent height={'20vh'} />
+        ) : (
+          <>
+            <RegisterForm state={userInput} onChange={handleChange} />
+            <Button
+              style={styles.btnFilledPurple}
+              onClick={handleRegister}
+              icon={''}
+            >
+              Register
+            </Button>
+            <div style={{ position: 'relative' }}>
+              <img
+                src={'../google.jpg'}
+                alt={''}
+                style={{
+                  height: 'calc(29px + 1vh)',
+                  width: 'calc(29px + 1vh)',
+                  position: 'absolute',
+                  left: '0',
+                  bottom: '0',
+                  padding: '0px',
+                  margin: '25px 0px 5px 0px',
+                  border: 'solid 1px #520369',
+                  borderRadius: '5px',
+                }}
+              />
+              <Button
+                style={styles.btnFilledPurple}
+                onClick={signInWithGoogle}
+                icon={''}
+              >
+                sign up with Google
+              </Button>
+            </div>
+          </>
+        )}
       </div>
       <Footer defaultButtons />
     </>
